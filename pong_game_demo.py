fef12ebc48cec7eb4fd6a31de77c80879e8354df
chiyu_lee <chiyu.lee3@gmail.com>
2023-07-31 11:02:25 +0200

    update the code

---
+++ pong_game_demo.py
@@ -0,0 +1,356 @@
+import pygame
+import cv2
+import numpy as np
+from collections import deque
+from track_chatGPT import color_check
+update_color_config=False
+if update_color_config:
+    lower_blue, upper_blue = color_check()
+    print(lower_blue, upper_blue)
+    lower_red, upper_red = color_check()
+    print(lower_red, upper_red)
+    lower_ranges = [np.array(lower_blue), np.array(lower_red)]
+    upper_ranges = [np.array(upper_blue), np.array(upper_red)]
+else:
+    lower_ranges = [np.array([96, 91, 83]), np.array([0, 80, 0])]
+    upper_ranges = [np.array([158, 255, 179]), np.array([78, 255, 179])]
+
+# print(lower_ranges, upper_ranges)
+cap = cv2.VideoCapture(0)
+pygame.init()
+
+# Basic parameters of the screen
+WIDTH, HEIGHT = 900, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+# Colors
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
+GREEN = pygame.Color(0, 255, 0)
+RED = pygame.Color(255, 0, 0)
+
+# Font that is used to render the text
+font20 = pygame.font.Font("freesansbold.ttf", 20)
+
+pts = deque(maxlen=10)
+counter = 0
+
+
+class Striker:
+    def __init__(self, posx, posy, width, height, speed, color):
+        self._posx = posx
+        self._posy = posy
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.color = color
+        self.geek_rect = pygame.Rect(posx, posy, width, height)
+
+    @property
+    def posy(self):
+        return self._posy
+
+    @posy.setter
+    def posy(self, value):
+        self._posy = max(0, min(value, HEIGHT - self.height))
+        self.geek_rect.y = self._posy
+
+    def display(self):
+        pygame.draw.rect(screen, self.color, self.geek_rect)
+
+    def update(self, y_fac):
+        self.posy += self.speed * y_fac
+
+    def display_score(self, text, score, x, y, color):
+        text = font20.render(text + str(score), True, color)
+        text_rect = text.get_rect(center=(x, y))
+        screen.blit(text, text_rect)
+
+    def get_rect(self):
+        return self.geek_rect
+
+
+class Ball:
+    def __init__(self, posx, posy, radius, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.radius = radius
+        self.speed = speed
+        self.color = color
+        self.x_fac = 1
+        self.y_fac = -1
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius
+        )
+        self.first_time = True
+
+    def display(self):
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius
+        )
+
+    def update(self):
+        self.posx += self.speed * self.x_fac
+        self.posy += self.speed * self.y_fac
+
+        if self.posy <= 0 or self.posy >= HEIGHT:
+            self.y_fac *= -1
+
+        if self.posx <= 0 and self.first_time:
+            self.first_time = False
+            return -1
+        elif self.posx >= WIDTH and self.first_time:
+            self.first_time = False
+            return 1
+        else:
+            return 0
+
+    def reset(self):
+        self.posx = WIDTH // 2
+        self.posy = HEIGHT // 2
+        self.x_fac *= -1
+        self.first_time = True
+
+    def hit(self):
+        self.x_fac *= -1
+
+    def get_rect(self):
+        return self.ball
+
+
+def keyboard_controller(event, pygame):
+    y_fac, y_fac1 = 0, 0
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            y_fac = -1
+        if event.key == pygame.K_DOWN:
+            y_fac = 1
+        if event.key == pygame.K_LEFT:
+            y_fac1 = -1
+        if event.key == pygame.K_RIGHT:
+            y_fac1 = 1
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+            y_fac = 0
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            y_fac1 = 0
+    return [y_fac, y_fac1]
+
+
+def camera_controller(track1, track2):
+    y_fac = 0
+    if track2 > track1:
+        y_fac = 1
+    elif track2 < track1:
+        y_fac = -1
+    else:
+        y_fac = 0
+    return y_fac
+
+
+def camera_controller2(area_1, area_2, area1_init, area2_init,counter):
+    darea_list=[0,0]
+    pts.appendleft((int(area_1), int(area_2)))
+    for i in np.arange(1, len(pts)):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        # check to see if enough points have been accumulated in
+        # the buffer
+        if counter >= 10 and i == 1 and pts[-10] is not None:
+            darea_list=np.mean(pts,axis=0)
+
+    if darea_list[0] > area1_init:
+        y_fac = 1
+    elif darea_list[0] < area1_init:
+        y_fac = -1
+    else:
+        y_fac = 0
+
+    if darea_list[1] > area2_init:
+        y_fac1 = 1
+    elif darea_list[1] < area2_init:
+        y_fac1 = -1
+    else:
+        y_fac1 = 0
+    return [y_fac, y_fac1]
+
+
+def AI_controller(ball, geek):
+    y_fac = 0
+    if ball.posy > geek.posy and abs(ball.posy - geek.posy) > 10:
+        y_fac = 1
+    elif ball.posy < geek.posy and abs(ball.posy - geek.posy) > 10:
+        y_fac = -1
+
+    return y_fac
+
+def AI_controller2(ball1,ball2, geek):
+    y_fac = 0
+    if ball1.posx > ball2.posx:
+        if ball2.posy > geek.posy and abs(ball2.posy - geek.posy) > 10:
+            y_fac = 1
+        elif ball2.posy < geek.posy and abs(ball2.posy - geek.posy) > 10:
+            y_fac = -1
+
+    elif ball1.posx < ball2.posx:
+        if ball1.posy > geek.posy and abs(ball1.posy - geek.posy) > 10:
+            y_fac = 1
+        elif ball1.posy < geek.posy and abs(ball1.posy - geek.posy) > 10:
+            y_fac = -1  
+    else:
+        y_fac=0
+
+    return y_fac
+
+
+def color_track(img, lower_range, upper_range):
+    num_cnt = 0
+    area = 0
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+    _, mask1 = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for c in cnts:
+        x = 600
+        if cv2.contourArea(c) > x:
+            area += cv2.contourArea(c)
+            num_cnt += 1
+    return num_cnt, area
+
+
+def main(game_modes):
+    running = True
+
+    geek1 = Striker(20, 0, 10, 100, 10, GREEN)
+    geek2 = Striker(WIDTH - 30, 0, 10, 100, 10, GREEN)
+    ball = Ball(WIDTH // 2, HEIGHT // 2, 7, 3.5, WHITE)
+    ball2 = (
+        Ball(WIDTH // 2, HEIGHT // 2, 7, 5, RED)
+        if game_modes.get("two_balls")
+        else None
+    )
+
+    list_of_geeks = [geek1, geek2]
+    geek1_score, geek2_score = 0, 0
+    geek1_y_fac, geek2_y_fac = 0, 0
+    area1_init=0
+    area2_init=0
+    while running:
+        screen.fill(BLACK)
+        if game_modes.get("play_with_camera"):
+            ret, frame = cap.read()
+            if not ret:
+                running = False
+                break
+            frame = cv2.resize(frame, (640, 480))
+            num_1, area_1 = color_track(frame, lower_ranges[0], upper_ranges[0])
+            num_2, area_2 = color_track(frame, lower_ranges[1], upper_ranges[1])
+            if counter==0:
+                area1_init=area_1
+                area2_init=area_2
+            if game_modes.get("one_player") == True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+
+                    geek2_y_fac = camera_controller(num_1, num_2)
+            # AI PC
+                geek1_y_fac = AI_controller(ball, geek1)
+                if game_modes.get(
+                    "two_balls"
+                ):  ### leave this as a bug for the tutorials: AI controller only cares about ball2
+                    geek1_y_fac = AI_controller(ball2, geek1)
+            else:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    ## there is a bug here. Keyboard controller2 is not functioning
+                # geek2_y_fac = keyboard_controller(event, pygame)
+                # geek2_y_fac, geek1_y_fac = keyboard_controller2(event, pygame)
+                    y_list = camera_controller2(area_1, area_2, area1_init, area2_init,counter)
+                    geek2_y_fac = y_list[0]
+                    geek1_y_fac = y_list[1]
+        else:
+            if game_modes.get("one_player") == True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    y_list = keyboard_controller(event, pygame)
+                    geek2_y_fac = y_list[0]
+            # AI PC
+
+                if game_modes.get(
+                    "two_balls"
+                ):  ### leave this as a bug for the tutorials: AI controller only cares about ball2
+                    geek1_y_fac = AI_controller2(ball, ball2, geek1)
+                else:
+                    geek1_y_fac = AI_controller(ball, geek1)
+            else:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    ## there is a bug here. Keyboard controller2 is not functioning
+
+                    y_list = keyboard_controller(event, pygame)
+                    geek2_y_fac = y_list[0]
+                    geek1_y_fac = y_list[1]
+
+        ##update the position of the paddles
+        geek2.update(geek2_y_fac)
+        geek1.update(geek1_y_fac)
+
+        ##collide rules of balls
+        for geek in list_of_geeks:
+            if pygame.Rect.colliderect(ball.get_rect(), geek.get_rect()):
+                ball.hit()
+            if game_modes.get("two_balls") and pygame.Rect.colliderect(
+                ball2.get_rect(), geek.get_rect()
+            ):
+                ball2.hit()
+
+        ##update the position of the balls
+        point1 = ball.update()
+        point2 = ball2.update() if game_modes.get("two_balls") else None
+
+        if point1 == -1:
+            geek2_score += 1
+        elif point1 == 1:
+            geek1_score += 1
+
+        if game_modes.get("two_balls") and point2:
+            if point2 == -1:
+                geek2_score += 1
+            elif point2 == 1:
+                geek1_score += 1
+
+        if point1:
+            ball.reset()
+        if game_modes.get("two_balls") and point2:
+            ball2.reset()
+
+        geek1.display()
+        geek2.display()
+        ball.display()
+        if game_modes.get("two_balls"):
+            ball2.display()
+
+        geek1.display_score("Konstanz Gamer : ", geek1_score, 100, 20, WHITE)
+        geek2.display_score("Collective Power : ", geek2_score, WIDTH - 100, 20, WHITE)
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    game_modes = {
+        "two_balls": True,
+        "one_player": True,
+        "play_with_camera": False,
+        "demo_mode": True,
+    }
+    main(game_modes)
+    pygame.quit()
--- test_chatGPT.py
+++ test_chatGPT.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy as np
 from collections import deque
+import matplotlib
+matplotlib.use('TKAgg')
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import time
+#from drawnow import *
 # from track_chatGPT import color_check
 # lower_blue, upper_blue = color_check()
 # print(lower_blue, upper_blue)
@@ -8,13 +14,46 @@ from collections import deque
 # print(lower_red, upper_red)
 # lower_ranges = [np.array(lower_blue), np.array(lower_red)]
 # upper_ranges = [np.array(upper_blue), np.array(upper_blue)]
-lower_ranges = [np.array([96, 91, 83]), np.array([ 0, 80,  0])]
+lower_ranges = [np.array([96, 91, 83]), np.array([ 0, 80,  0])]#red color
 upper_ranges = [np.array([158, 255, 179]), np.array([78, 255, 179])]
 pts = deque(maxlen=32)
 counter=0
 
-
 cap = cv2.VideoCapture(0)
+
+def update(data):
+    # Get the current elapsed time
+    elapsed_time = time.time() - start_time
+
+    # Append the new data to the lists
+    times.append(elapsed_time)
+    values.append(data)
+
+    # Limit the lists to show only the last 10 data points
+    times_ = times[-20:]
+    values_ = values[-20:]
+
+    # Clear previous plot
+    plt.clf()
+
+    # Plot the data
+    plt.plot(times_, values_, marker='o')
+
+    # Add labels and title (optional)
+    plt.xlabel('Elapsed Time (s)')
+    plt.ylabel('Random Value')
+    plt.title('Stopwatch-style Live Line Chart')
+
+# Create empty lists to store data
+times = []
+values = []
+start_time = time.time()
+# Get the start time
+
+
+
+
+
 
 
 def color(img, lower_range, upper_range):
@@ -55,7 +94,10 @@ def camera_controller2(area_1, area_2):
             # text variables
             darea_1 = pts[-10][0] - pts[i][0]
             darea_2 = pts[-10][1] - pts[i][1]
-            print(darea_2)
+
+        # if counter >=20:                            #If you have 50 or more points, delete the first one from the array
+        #     darea_2.pop(0)                       #This allows us to just see the last 50 data points
+        #     darea_1.pop(0)
     # if darea_1 > 0:
     #     print("area1 decreases")
     # elif darea_1 < 0:
@@ -69,6 +111,12 @@ def camera_controller2(area_1, area_2):
     #     print("area2 increases")
     # else:
     #     print("area2 stay the same")
+fig, ax = plt.subplots()
+
+    # Create the animation
+ani = FuncAnimation(fig, update, frames=100, interval=10)  # 100 frames, update every 100 milliseconds (0.1 second)
+
+    # Show the plot
 
 while True:
     ret, frame = cap.read()
@@ -77,11 +125,15 @@ while True:
     num_2,area_2 = color(frame, lower_ranges[1], upper_ranges[1])
     #print(f"first color number{num_1}, area{area_1}")
     #print(f"first color number{num_2}, area{area_2}")
+    update(area_1)
+
     camera_controller2(area_1, area_2)
     counter +=1
     cv2.imshow("FRAME", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
+
 cap.release()
 cv2.destroyAllWindows()
+plt.show()
